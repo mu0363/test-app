200a633485d21d47d014db50efc813f4b33bdb8c
mu0363 <dde036@gmail.com>
2022-02-01 14:11:15 +0900

    all setup

--- src/hooks/useAuth.tsx
+++ src/hooks/useAuth.tsx
@@ -58,45 +58,45 @@ export const useAuth = () => {
     setUser(newUser);
   };
 
-  const updateAvatar = async (event: ChangeEvent<HTMLInputElement>) => {
-    try {
-      if (!event.target.files || event.target.files.length === 0) {
-        throw new Error("You must select an image to upload.");
-      }
-      if (user) {
-        const previousAvatarUrl = user?.avatar_url;
-        const previousAvatarFileName = (previousAvatarUrl.match(
-          ".+/(.+?)([?#;].*)?$"
-        ) ?? "")[1];
+  // const updateAvatar = async (event: ChangeEvent<HTMLInputElement>) => {
+  //   try {
+  //     if (!event.target.files || event.target.files.length === 0) {
+  //       throw new Error("You must select an image to upload.");
+  //     }
+  //     if (user) {
+  //       const previousAvatarUrl = user?.avatar_url;
+  //       const previousAvatarFileName = (previousAvatarUrl.match(
+  //         ".+/(.+?)([?#;].*)?$"
+  //       ) ?? "")[1];
 
-        // storage に画像をアップロード
-        const file = event.target.files[0];
-        const fileExt = file.name.split(".").pop();
-        const nanoFileName = nanoid();
-        const fileName = `${nanoFileName}.${fileExt}`;
+  //       // storage に画像をアップロード
+  //       const file = event.target.files[0];
+  //       const fileExt = file.name.split(".").pop();
+  //       const nanoFileName = nanoid();
+  //       const fileName = `${nanoFileName}.${fileExt}`;
 
-        await supabase.storage
-          .from("avatars")
-          .upload(`private/${fileName}`, file);
+  //       await supabase.storage
+  //         .from("avatars")
+  //         .upload(`private/${fileName}`, file);
 
-        const url = await supabase.storage
-          .from("avatars")
-          .getPublicUrl(`private/${fileName}`).publicURL;
-        const { data: newUser } = await supabase
-          .from("users")
-          .update({ avatar_url: url, updated_at: new Date() })
-          .match({ id: user?.id })
-          .single();
-        setUser(newUser);
+  //       const url = await supabase.storage
+  //         .from("avatars")
+  //         .getPublicUrl(`private/${fileName}`).publicURL;
+  //       const { data: newUser } = await supabase
+  //         .from("users")
+  //         .update({ avatar_url: url, updated_at: new Date() })
+  //         .match({ id: user?.id })
+  //         .single();
+  //       setUser(newUser);
 
-        await supabase.storage
-          .from("avatars")
-          .remove([`private/${previousAvatarFileName}`]);
-      }
-    } catch (error) {
-      alert(error);
-    }
-  };
+  //       await supabase.storage
+  //         .from("avatars")
+  //         .remove([`private/${previousAvatarFileName}`]);
+  //     }
+  //   } catch (error) {
+  //     alert(error);
+  //   }
+  // };
 
   return {
     session,
