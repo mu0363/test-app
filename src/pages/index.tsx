be6abdc187a8b0797aefc650b7e3ca4e493fbd43
mu0363 <dde036@gmail.com>
2022-02-01 14:04:11 +0900

    all setup

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,15 +5,15 @@ import { useAuth } from "@/hooks/useAuth";
 
 const Home: NextPage = () => {
   const [isLoading, setIsLoading] = useState(false);
-  const { user, signIn, signOut } = useAuth();
+  // const { user, signIn, signOut } = useAuth();
 
-  console.log(user);
+  // console.log(user);
 
-  const handleSignIn = (e: MouseEvent<HTMLButtonElement>) => {
-    e.preventDefault();
-    setIsLoading(true);
-    signIn();
-  };
+  // const handleSignIn = (e: MouseEvent<HTMLButtonElement>) => {
+  //   e.preventDefault();
+  //   setIsLoading(true);
+  //   signIn();
+  // };
 
   return (
     <div className="text-center selection:text-yellow-500 selection:bg-yellow-300">
@@ -21,9 +21,9 @@ const Home: NextPage = () => {
         <h1 className="text-2xl font-bold underline decoration-yellow-500">
           Supabase + Next.js
         </h1>
-        <p className="font-bold">{user?.full_name}</p>
+        {/* <p className="font-bold">{user?.full_name}</p> */}
       </div>
-      <button
+      {/* <button
         className="py-4 px-6 mx-auto mr-5 mb-10 text-white bg-gradient-to-r from-cyan-500 to-blue-500 rounded-full hover:shadow-lg hover:shadow-blue-500/50 transition hover:scale-105"
         onClick={handleSignIn}
         disabled={isLoading}
@@ -31,7 +31,7 @@ const Home: NextPage = () => {
         <span>{isLoading ? "Loading" : "Sign in with Google"}</span>
       </button>
 
-      <button onClick={() => signOut()}>Sign out</button>
+      <button onClick={() => signOut()}>Sign out</button> */}
     </div>
   );
 };
